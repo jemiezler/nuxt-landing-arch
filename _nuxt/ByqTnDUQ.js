4b4aac99a6d5c1382189c261a5bb7803d58c7adb
Nattawat Nattachanasit <6531503201@lamduan.mfu.ac.th>
2025-01-30 20:38:06 +0700

    Updates

---
+++ _nuxt/ByqTnDUQ.js
@@ -0,0 +1,1 @@
+import{_ as c,c as e,o as t}from"./DerRAk5V.js";const n={};function o(r,s){return t(),e("p",null,"This is Certifications")}const a=c(n,[["render",o]]);export{a as default};
---
+++ _nuxt/mpT1nxBj.js
@@ -0,0 +1,1 @@
+import{_ as e,c,o as r}from"./DerRAk5V.js";const n={};function o(s,t){return r(),c("p",null,"This is Gallery")}const l=e(n,[["render",o]]);export{l as default};
