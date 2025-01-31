1158a27bc96761df509aa6a4aa7d8b8d12bb0208
Nattawat Nattachanasit <6531503201@lamduan.mfu.ac.th>
2025-01-31 14:34:20 +0700

    add Vercel Speed Insights integration and update dependencies

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,5 +3,9 @@
     <div class="pt-24">
         <slot />
     </div>
+    <SpeedInsights/>
     <AppFooter />
 </template>
+<script setup lang="ts">
+import { SpeedInsights } from "@vercel/speed-insights/nuxt"
+</script>
