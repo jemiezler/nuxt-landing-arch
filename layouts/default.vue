b70a023176f9e256ec8e617adb5d1eaaa8697a16
Nattawat Nattachanasit <6531503201@lamduan.mfu.ac.th>
2025-02-02 21:47:11 +0700

    fix: update SpeedInsights import to use Vue version and adjust template placement

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,12 @@
 <template>
+    <SpeedInsights />
 
     <div class="grid pt-24 min-w-screen min-h-screen">
         <AppHeader />
         <slot />
         <AppFooter />
     </div>
-    <SpeedInsights />
 </template>
 <script setup lang="ts">
-import { SpeedInsights } from "@vercel/speed-insights/nuxt"
+import { SpeedInsights } from '@vercel/speed-insights/vue';
 </script>
